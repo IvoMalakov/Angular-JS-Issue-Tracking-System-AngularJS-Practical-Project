498056061938b4f591878954f9f246b8f3990b9b
onefen <ivo.malakov85@gmail.com>
2016-04-26 20:45:56 +0300

    I added Edit Issue Controller and Edit Issue View. Users can now edit their Issues.

--- app/js/controllers/editIssue-controller.js
+++ app/js/controllers/editIssue-controller.js
@@ -8,5 +8,45 @@ issueTrackerSystem.controller('EditIssueController', [
     'notificationService',
     function($scope, $location, $routeParams, issueService, notificationService) {
 
+        var convertLabels = function toObject(inputArray) {
+            var outputArrayAsJson = [];
+
+            inputArray.forEach(function(element) {
+                outputArrayAsJson.push({Name: element});
+            });
+
+            return outputArrayAsJson;
+        };
+
+        function getIssueById(id) {
+            issueService.getIssueById(id)
+                .then(function(response) {
+                    $scope.issue = response.data;
+                }, function(error) {
+                    notificationService.showError('Request failed', error.statusText);
+                });
+        }
+
+        getIssueById($routeParams.id);
+
+        $scope.editIssue = function editIssue(issue) {
+            var issueForEdit = {
+                Title: issue.Title,
+                Description: issue.Description,
+                DueDate: issue.DueDate,
+                ProjectId: parseInt($scope.issue.Project.Id),
+                AssigneeId: issue.AssigneeId,
+                PriorityId: issue.PriorityId,
+                labels: convertLabels(issue.Labels)
+            };
+
+            issueService.editIssue(issueForEdit, $routeParams.id)
+                .then(function() {
+                    notificationService.showInfo('Issue edited successful');
+                    $location.path('/issues' + $routeParams.id);
+                }, function(error) {
+                    notificationService.showError('Edit issue failed', error.statusText);
+                });
+        }
     }
 ]);
